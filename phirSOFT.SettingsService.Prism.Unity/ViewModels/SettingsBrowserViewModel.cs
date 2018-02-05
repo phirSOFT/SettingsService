466ecce01320a5efdef57b78ec1f0d880a05cdb5
Philemon Eichin <phileicht@gmx.de>
2018-02-05 22:21:53 +0100

    Made RootNodes Lazy

--- phirSOFT.SettingsService.Prism.Unity/ViewModels/SettingsBrowserViewModel.cs
+++ phirSOFT.SettingsService.Prism.Unity/ViewModels/SettingsBrowserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -12,13 +13,9 @@ namespace phirSOFT.SettingsService.Prism.Unity.ViewModels
     internal class SettingsBrowserViewModel : BindableBase
     {
         private string _filter;
+        private readonly Lazy<ICollection<SettingsNodeViewModel>> _rootNodes = new Lazy<ICollection<SettingsNodeViewModel>>(() => ServiceLocator.Current.GetAllInstances<ISettingPage>().BuildTree(SettingsNodeViewModel.Create));
 
-        public SettingsBrowserViewModel()
-        {
-            RootNodes = ServiceLocator.Current.GetAllInstances<ISettingPage>().BuildTree(SettingsNodeViewModel.Create);
-        }
-
-        public ICollection<SettingsNodeViewModel> RootNodes { get; }
+        public ICollection<SettingsNodeViewModel> RootNodes => _rootNodes.Value;
 
         public string Filter
         {
