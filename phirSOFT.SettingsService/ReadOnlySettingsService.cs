7f0fae8f26b069c47a562a4f455ef7c6901020e1
Philemon Eichin <phileicht@gmx.de>
2018-02-10 13:22:08 +0100

    Add ReadOnlyWrapper

---
+++ phirSOFT.SettingsService/ReadOnlySettingsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phirSOFT.SettingsService
+{
+    public class ReadOnlySettingsService : IReadOnlySettingsService
+    {
+        private readonly IReadOnlySettingsService _service;
+
+        public ReadOnlySettingsService(IReadOnlySettingsService service)
+        {
+            this._service = service;
+        }
+
+        public Task<T> GetSettingAsync<T>(string key)
+        {
+            return _service.GetSettingAsync<T>(key);
+        }
+
+        public Task<object> GetSettingAsync(string key, Type type)
+        {
+            return _service.GetSettingAsync(key, type);
+        }
+
+        public Task<bool> IsRegisterdAsync(string key)
+        {
+            return _service.IsRegisterdAsync(key);
+        }
+    }
+}
---
+++ phirSOFT.SettingsService/SettingsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace phirSOFT.SettingsService
+{
+    public static class SettingsService
+    {
+        public static ReadOnlySettingsService AsReadOnly(this IReadOnlySettingsService service) => new ReadOnlySettingsService(service);
+    }
+}
