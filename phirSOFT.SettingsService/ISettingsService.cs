08de5bc7f93c8c956386a7b3b766e37a96593f74
Philemon Eichin <phileicht@gmx.de>
2018-02-10 14:50:38 +0100

    Remove old Methods

--- phirSOFT.SettingsService/ISettingsService.cs
+++ phirSOFT.SettingsService/ISettingsService.cs
@@ -14,23 +14,6 @@ namespace phirSOFT.SettingsService
     /// </remarks>
     public interface ISettingsService : IReadOnlySettingsService
     {
-        /// <summary>
-        ///     Sets a setting to a new value.
-        /// </summary>
-        /// <typeparam name="T">The type of the setting.</typeparam>
-        /// <param name="key">The key of the setting.</param>
-        /// <param name="value">The value to set.</param>
-        Task SetSettingAsync<T>(string key, T value);
-
-        /// <summary>
-        ///     Sets a setting to a new value.
-        /// </summary>
-        /// <param name="key">The key of the setting</param>
-        /// <param name="value">The value of the setting</param>
-        /// <remarks>
-        ///     The settings type will be determined automatically. This can lead to unexpected results.
-        /// </remarks>
-        Task SetSettingAsync(string key, object value);
 
         /// <summary>
         ///     Sets a setting to a new value.
@@ -39,66 +22,6 @@ namespace phirSOFT.SettingsService
         /// <param name="value">he value of the setting.</param>
         /// <param name="type">The type of the setting.</param>
         Task SetSettingAsync(string key, object value, Type type);
-
-        /// <summary>
-        ///     Registers a setting in this service
-        /// </summary>
-        /// <typeparam name="T">The type of the setting</typeparam>
-        /// <param name="key">The key of the setting</param>
-        Task RegisterSettingAsync<T>(string key);
-
-        /// <summary>
-        ///     Registers a setting in this service
-        /// </summary>
-        /// <param name="key">The key of the setting</param>
-        /// <param name="type">The type of the setting</param>
-        Task RegisterSettingAsync(string key, Type type);
-
-        /// <summary>
-        ///     Registers a setting with a default value in this service .
-        /// </summary>
-        /// <typeparam name="T">The type of the setting</typeparam>
-        /// <param name="key">The key of this setting</param>
-        /// <param name="defaultValue">The value of this setting</param>
-        Task RegisterSettingAsync<T>(string key, T defaultValue);
-
-        /// <summary>
-        ///     Registers a setting with a default value in this service
-        /// </summary>
-        /// <param name="key">The key of this setting</param>
-        /// <param name="defaultValue">The default value of this setting</param>
-        /// <remarks>
-        ///     The settings type will be determined automatically. This can lead to unexpected results.
-        /// </remarks>
-        Task RegisterSettingAsync(string key, object defaultValue);
-
-        /// <summary>
-        ///     Registers a setting with a default value in this service
-        /// </summary>
-        /// <param name="key">The key of this setting</param>
-        /// <param name="defaultValue">The default value of this setting</param>
-        /// <param name="type">The type of the setting.</param>
-        Task RegisterSettingAsync(string key, object defaultValue, Type type);
-
-        /// <summary>
-        ///     Registers a setting with a default and an initial value in this service.
-        /// </summary>
-        /// <typeparam name="T">The type of the setting.</typeparam>
-        /// <param name="key">The key of this setting.</param>
-        /// <param name="defaultValue">The default value of this setting.</param>
-        /// <param name="initialValue">The initial value of this setting.</param>
-        Task RegisterSettingAsync<T>(string key, T defaultValue, T initialValue);
-
-        /// <summary>
-        ///     Registers a setting with a default and an initial value in this service.
-        /// </summary>
-        /// <param name="key">The key of this setting.</param>
-        /// <param name="defaultValue">The default value of this setting.</param>
-        /// <param name="initialValue">The initial value of this setting.</param>
-        /// <remarks>
-        ///     The settings type will be determined automatically. This can lead to unexpected results.
-        /// </remarks>
-        Task RegisterSettingAsync(string key, object defaultValue, object initialValue);
 
         /// <summary>
         ///     Registers a setting with a default and an initial value in this service.
