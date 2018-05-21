2cc1eb5133be7362f3969ffb3dc611f318d0d805
Philemon Eichin <phileicht@gmx.de>
2018-05-21 11:11:55 +0200

    Add SettingsValueAttribute
    This Attribute is used to tell the unity container, to resolve the value of a parameter or property from a settings service.

---
+++ phirSOFT.SettingsService.Unity/SettingValueAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace phirSOFT.SettingsService.Unity
+{
+    /// <summary>
+    /// Use this attribute, to annotate, that the property or parameter is resolved from a unity container.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
+    public class SettingValueAttribute : Attribute
+    {
+        public SettingValueAttribute(string settingKey, string serviceInstance = "", Type settingType = null)
+        {
+            SettingKey = settingKey;
+            ServiceInstance = serviceInstance;
+            SettingType = settingType;
+        }
+
+        public string ServiceInstance { get; }
+        public string SettingKey { get; }
+        public Type SettingType { get; }
+
+    }
+}
