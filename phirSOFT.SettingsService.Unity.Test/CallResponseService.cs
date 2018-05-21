e479845400eede8b81c905dcd46ea2ae6ba4f944
Philemon Eichin <phileicht@gmx.de>
2018-05-21 12:14:56 +0200

    Add UnitTests

---
+++ phirSOFT.SettingsService.Unity.Test/CallResponseService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using phirSOFT.SettingsService;
+
+namespace phirSOFT.SettingsService.Unity.Test
+{
+    internal class CallResponseService : ISettingsService
+    {
+        public Task<object> GetSettingAsync(string key, Type type)
+        {
+            if (type != typeof(string))
+                return Task.FromResult(type.IsValueType ? Activator.CreateInstance(type) : null);
+            return Task.FromResult((object) key);
+        }
+
+        public Task<bool> IsRegisterdAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetSettingAsync(string key, object value, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterSettingAsync(string key, object defaultValue, object initialValue, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UnregisterSettingAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DiscardAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ phirSOFT.SettingsService.Unity.Test/TestClass.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Injection;
+using Unity.Registration;
+
+namespace phirSOFT.SettingsService.Unity.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var container = new UnityContainer();
+            container.AddNewExtension<SettingsServiceContainerExtension>();
+
+            container.RegisterType<IReadOnlySettingsService, ISettingsService>();
+            container.RegisterType<ISettingsService, CallResponseService>();
+
+            var instance = container.Resolve<TestSampleClass>();
+
+            Assert.AreEqual("test", instance.Test);
+            Assert.AreEqual("Test1", instance.Test1);
+        }
+    }
+
+    internal class TestSampleClass
+    {
+        [global::Unity.Attributes.Dependency]
+        [SettingValue("Test1")]
+        public string Test1 { get; set; }
+
+        public string Test { get; }
+
+        public TestSampleClass([SettingValue("test")]string test)
+        {
+            Test = test;
+        }
+    }
+}
