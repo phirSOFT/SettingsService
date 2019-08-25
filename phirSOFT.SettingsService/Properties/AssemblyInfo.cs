4a06e22cd3f980015226ac1e098efc60bac18c1d
Philemon Eichin <philemon.eichin@t4dt.com>
2019-08-25 21:44:44 +0200

    Add AssenblyInfo

---
+++ phirSOFT.SettingsService/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="AssemblyInfo.cs" company="phirSOFT">
+// Copyright (c) phirSOFT. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("phirSOFT.SettingsService.Test")]
