4688ee481f77119c841a69c440ff249af3ad71f9
Philemon Eichin <philemon.eichin@t4dt.com>
2019-08-25 21:57:41 +0200

    Add Test for TypeHelper.GetDefaultValue

---
+++ phirSOFT.SettingsService.Test/TypeHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace phirSOFT.SettingsService.Test
+{
+    [TestFixture]
+    public class TypeHelperTest
+    {
+        [Test()]
+        [TestCase(typeof(object), default(object))]
+        [TestCase(typeof(string), default(string))]
+        [TestCase(typeof(byte), default(byte))]
+        [TestCase(typeof(sbyte), default(sbyte))]
+        [TestCase(typeof(short), default(short))]
+        [TestCase(typeof(ushort), default(ushort))]
+        [TestCase(typeof(int), default(int))]
+        [TestCase(typeof(uint), default(uint))]
+        [TestCase(typeof(long), default(long))]
+        [TestCase(typeof(ulong), default(ulong))]
+        [TestCase(typeof(float), default(float))]
+        [TestCase(typeof(double), default(double))]
+        public void TestDefaultType(Type type, object expectedDefaultValue)
+        {
+            object defaultValue = TypeHelper.GetDefaultValue(type);
+            Assert.AreEqual(expectedDefaultValue, defaultValue);
+        }
+    }
+}
