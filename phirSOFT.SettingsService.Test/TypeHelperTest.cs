5dd4eed140492f22a967c9f37047fcf441e458f2
Philemon Eichin <philemon.eichin@t4dt.com>
2019-08-25 22:20:46 +0200

    Test AreAssignable

--- phirSOFT.SettingsService.Test/TypeHelperTest.cs
+++ phirSOFT.SettingsService.Test/TypeHelperTest.cs
@@ -137,5 +137,101 @@ namespace phirSOFT.SettingsService.Test
                 );
             Assert.AreEqual(abCommon, baCommon);
         }
+
+        [Test]
+        [TestCase(typeof(Root), typeof(Root), typeof(Root))]
+        [TestCase(typeof(Root), typeof(A), typeof(Root))]
+        [TestCase(typeof(Root), typeof(B), typeof(Root))]
+        [TestCase(typeof(Root), typeof(A1), typeof(Root))]
+        [TestCase(typeof(Root), typeof(A2), typeof(Root))]
+        [TestCase(typeof(Root), typeof(B1), typeof(Root))]
+        [TestCase(typeof(Root), typeof(B2), typeof(Root))]
+        [TestCase(typeof(Root), typeof(Other), null)]
+        [TestCase(typeof(A), typeof(A), typeof(A))]
+        [TestCase(typeof(A), typeof(B), null)]
+        [TestCase(typeof(A), typeof(A1), typeof(A))]
+        [TestCase(typeof(A), typeof(A2), typeof(A))]
+        [TestCase(typeof(A), typeof(B1), null)]
+        [TestCase(typeof(A), typeof(B2), null)]
+        [TestCase(typeof(A), typeof(Other), null)]
+        [TestCase(typeof(B), typeof(B), typeof(B))]
+        [TestCase(typeof(B), typeof(A1), null)]
+        [TestCase(typeof(B), typeof(A2), null)]
+        [TestCase(typeof(B), typeof(B1), typeof(B))]
+        [TestCase(typeof(B), typeof(B2), typeof(B))]
+        [TestCase(typeof(B), typeof(Other), null)]
+        [TestCase(typeof(A1), typeof(A1), typeof(A1))]
+        [TestCase(typeof(A1), typeof(A2), null)]
+        [TestCase(typeof(A1), typeof(B1), null)]
+        [TestCase(typeof(A1), typeof(B2), null)]
+        [TestCase(typeof(A1), typeof(Other), null)]
+        [TestCase(typeof(A2), typeof(A2), typeof(A2))]
+        [TestCase(typeof(A2), typeof(B1), null)]
+        [TestCase(typeof(A2), typeof(B2), null)]
+        [TestCase(typeof(A1), typeof(Other), null)]
+        [TestCase(typeof(B1), typeof(B1), typeof(B1))]
+        [TestCase(typeof(B1), typeof(B2), null)]
+        [TestCase(typeof(B1), typeof(Other), null)]
+        [TestCase(typeof(B2), typeof(B2), typeof(B2))]
+        [TestCase(typeof(B2), typeof(Other), null)]
+        [TestCase(typeof(Other), typeof(Other), typeof(Other))]
+        public void TestAreAssignable(Type a, Type b, Type expectedAssignment)
+        {
+            if (expectedAssignment == null)
+            {
+                Assert.IsFalse(TypeHelper.AreAssignable(a.GetTypeInfo(), b.GetTypeInfo(), out _));
+            }
+            else
+            {
+                Assert.IsTrue(TypeHelper.AreAssignable(a.GetTypeInfo(), b.GetTypeInfo(), out Type common));
+                Assert.AreEqual(expectedAssignment, common);
+            }
+        }
+
+        [Test]
+        [TestCase(typeof(Root), typeof(Root))]
+        [TestCase(typeof(Root), typeof(A))]
+        [TestCase(typeof(Root), typeof(B))]
+        [TestCase(typeof(Root), typeof(A1))]
+        [TestCase(typeof(Root), typeof(A2))]
+        [TestCase(typeof(Root), typeof(B1))]
+        [TestCase(typeof(Root), typeof(B2))]
+        [TestCase(typeof(Root), typeof(Other))]
+        [TestCase(typeof(A), typeof(A))]
+        [TestCase(typeof(A), typeof(B))]
+        [TestCase(typeof(A), typeof(A1))]
+        [TestCase(typeof(A), typeof(A2))]
+        [TestCase(typeof(A), typeof(B1))]
+        [TestCase(typeof(A), typeof(B2))]
+        [TestCase(typeof(A), typeof(Other))]
+        [TestCase(typeof(B), typeof(B))]
+        [TestCase(typeof(B), typeof(A1))]
+        [TestCase(typeof(B), typeof(A2))]
+        [TestCase(typeof(B), typeof(B1))]
+        [TestCase(typeof(B), typeof(B2))]
+        [TestCase(typeof(B), typeof(Other))]
+        [TestCase(typeof(A1), typeof(A1))]
+        [TestCase(typeof(A1), typeof(A2))]
+        [TestCase(typeof(A1), typeof(B1))]
+        [TestCase(typeof(A1), typeof(B2))]
+        [TestCase(typeof(A1), typeof(Other))]
+        [TestCase(typeof(A2), typeof(A2))]
+        [TestCase(typeof(A2), typeof(B1))]
+        [TestCase(typeof(A2), typeof(B2))]
+        [TestCase(typeof(A1), typeof(Other))]
+        [TestCase(typeof(B1), typeof(B1))]
+        [TestCase(typeof(B1), typeof(B2))]
+        [TestCase(typeof(B1), typeof(Other))]
+        [TestCase(typeof(B2), typeof(B2))]
+        [TestCase(typeof(B2), typeof(Other))]
+        [TestCase(typeof(Other), typeof(Other))]
+        public void TestAreAssignableSymmetrical(Type a, Type b)
+        {
+            Assert.AreEqual(
+                TypeHelper.AreAssignable(a.GetTypeInfo(), b.GetTypeInfo(), out Type abCommon),
+                TypeHelper.AreAssignable(b.GetTypeInfo(), a.GetTypeInfo(), out Type baCommon)
+                );
+            Assert.AreEqual(abCommon, baCommon);
+        }
     }
 }
