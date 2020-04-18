4f8bd1e9e9265d86387fa062b812b0a29052a065
Philemon Eichin <philemon.eichin@t4dt.com>
2020-04-18 11:52:00 +0200

    Add `ISettingMigration` interface

---
+++ phirSOFT.SettingsService.Abstractions/ISettingMigration.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ISettingMigration.cs" company="phirSOFT">
+// Copyright (c) phirSOFT. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace phirSOFT.SettingsService.Abstractions
+{
+    /// <summary>
+    /// Provides a definition how to migrate a set of settings.
+    /// </summary>
+    public interface ISettingMigration
+    {
+        /// <summary>
+        ///     Migrates the setting set from an older version of the settings set.
+        /// </summary>
+        /// <param name="settingsService">The <see cref="ISettingsService"/> to migrate.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to cancel the migration. </param>
+        /// <returns>A <see cref="Task"/>, that represents the asynchronous operation.</returns>
+        Task MigrateUpAsync(ISettingsService settingsService, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        ///     Removes the changes made by this <see cref="ISettingMigration"/> from a <see cref="ISettingsService"/>.
+        /// </summary>
+        /// <param name="settingsService">The <see cref="ISettingsService"/> to migrate.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to cancel the migration. </param>
+        /// <returns>A <see cref="Task"/>, that represents the asynchronous operation.</returns>
+        Task MigrateDownAsync(ISettingsService settingsService, CancellationToken cancellationToken = default);
+    }
+}
