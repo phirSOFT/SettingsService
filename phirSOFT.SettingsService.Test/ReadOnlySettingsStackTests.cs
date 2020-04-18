b929f95f37b3443302e0846a103d32652fdcfc6b
Restyled.io <commits@restyled.io>
2020-04-18 09:08:10 +0000

    Restyled by astyle

--- phirSOFT.SettingsService.Test/ReadOnlySettingsStackTests.cs
+++ phirSOFT.SettingsService.Test/ReadOnlySettingsStackTests.cs
@@ -6,40 +6,40 @@ using phirSOFT.SettingsService.Abstractions;
 
 namespace phirSOFT.SettingsService.Test
 {
-    [TestFixture]
-    public class ReadOnlySettingsStackTests
+[TestFixture]
+public class ReadOnlySettingsStackTests
+{
+    private const string Key1 = "key1";
+
+    [Test]
+    public async Task GetSettingAsync_ReturnsValue()
     {
-        private const string Key1 = "key1";
+        // Arrange
+        var readOnlySettingsStack = new ReadOnlySettingsStack { CreateReadOnlySettingsService() };
+        Type type = typeof(string);
 
-        [Test]
-        public async Task GetSettingAsync_ReturnsValue()
-        {
-            // Arrange
-            var readOnlySettingsStack = new ReadOnlySettingsStack { CreateReadOnlySettingsService() };
-            Type type = typeof(string);
+        // Act
+        object? result = await readOnlySettingsStack.GetSettingAsync(
+                             Key1,
+                             type);
 
-            // Act
-            object? result = await readOnlySettingsStack.GetSettingAsync(
-                Key1,
-                type);
-
-            // Assert
-            Assert.IsInstanceOf<string>(result);
-            Assert.AreEqual("value1", result);
-        }
-
-        [SetUp]
-        public void SetUp()
-        {
-        }
-
-        private static IReadOnlySettingsService CreateReadOnlySettingsService()
-        {
-            IReadOnlySettingsService settingsServiceMock = Substitute.For<IReadOnlySettingsService>();
-
-            settingsServiceMock.IsRegisteredAsync(Arg.Is(Key1)).Returns(true);
-            settingsServiceMock.GetSettingAsync(Arg.Is(Key1), Arg.Is(typeof(string))).Returns("value1");
-            return settingsServiceMock;
-        }
+        // Assert
+        Assert.IsInstanceOf<string>(result);
+        Assert.AreEqual("value1", result);
     }
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    private static IReadOnlySettingsService CreateReadOnlySettingsService()
+    {
+        IReadOnlySettingsService settingsServiceMock = Substitute.For<IReadOnlySettingsService>();
+
+        settingsServiceMock.IsRegisteredAsync(Arg.Is(Key1)).Returns(true);
+        settingsServiceMock.GetSettingAsync(Arg.Is(Key1), Arg.Is(typeof(string))).Returns("value1");
+        return settingsServiceMock;
+    }
+}
 }
