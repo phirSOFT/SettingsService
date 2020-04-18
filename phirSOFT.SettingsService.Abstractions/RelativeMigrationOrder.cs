047e75844bbf3ece1c350d7616468ba0d7a65085
Philemon Eichin <phileicht@gmx.de>
2020-04-18 12:15:54 +0200

    Add `RelativeMigrationOrder` enum

---
+++ phirSOFT.SettingsService.Abstractions/RelativeMigrationOrder.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="RelativeMigrationOrder.cs" company="phirSOFT">
+// Copyright (c) phirSOFT. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace phirSOFT.SettingsService.Abstractions
+{
+    /// <summary>
+    /// Determines in which order two <see cref="ISettingMigration"/> should be applied relative to each other.
+    /// </summary>
+    public enum RelativeMigrationOrder
+    {
+        /// <summary>
+        /// The annotated <see cref="ISettingsService"/> should be applied before the referenced.
+        /// </summary>
+        Before = -1,
+
+        /// <summary>
+        /// The annotated <see cref="ISettingsService"/> should be applied after the referenced.
+        /// </summary>
+        After = 1,
+    }
+}
